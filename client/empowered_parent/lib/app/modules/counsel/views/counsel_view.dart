39cea235af89cfd390c860f193670301048020f6
AdityaT-19 <beanadi1910@gmail.com>
2024-07-14 02:26:21 +0530

    parent done

--- client/empowered_parent/lib/app/modules/counsel/views/counsel_view.dart
+++ client/empowered_parent/lib/app/modules/counsel/views/counsel_view.dart
@@ -1,5 +1,6 @@
 import 'package:empowered_parent/app/modules/counsel/controllers/counsel_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class CounselView extends StatefulWidget {
   @override
@@ -7,7 +8,7 @@ class CounselView extends StatefulWidget {
 }
 
 class _CounselView extends State<CounselView> {
-  List<bool> _isExpanded = [false, false, false];
+  final counselController = Get.find<CounselController>();
 
   @override
   Widget build(BuildContext context) {
@@ -22,61 +23,74 @@ class _CounselView extends State<CounselView> {
         ),
         backgroundColor: Colors.deepPurple,
       ),
-      body: ListView(
-        padding: const EdgeInsets.all(16.0),
-        children: [
-          _buildReportCard(0, counsel_data[0].title, counsel_data[0].details),
-          SizedBox(height: 20.0),
-          _buildReportCard(1, counsel_data[1].title, counsel_data[1].details),
-          SizedBox(height: 20.0),
-          _buildReportCard(2, counsel_data[2].title, counsel_data[2].details),
-        ],
+      body: Obx(
+        () => counselController.isLoading.value
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : ListView(
+                padding: const EdgeInsets.all(16.0),
+                children: [
+                  for (var i = 0;
+                      i < counselController.counsel_data.length;
+                      i++)
+                    _buildReportCard(
+                      i,
+                      counselController.counsel_data[i].title,
+                      counselController.counsel_data[i].details,
+                    ),
+                ],
+              ),
       ),
     );
   }
 
   Widget _buildReportCard(int index, String title, String details) {
-    return Card(
-      elevation: 5,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15.0),
-      ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(15.0),
-        child: ExpansionPanelList(
-          expandedHeaderPadding: EdgeInsets.all(0),
-          elevation: 1,
-          expansionCallback: (int item, bool status) {
-            setState(() {
-              _isExpanded[index] = !_isExpanded[index];
-            });
-          },
-          children: [
-            ExpansionPanel(
-              backgroundColor: Colors.deepPurple.shade50,
-              headerBuilder: (BuildContext context, bool isExpanded) {
-                return ListTile(
-                  title: Text(
-                    title,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(15.0),
+          child: ExpansionPanelList(
+            expandedHeaderPadding: EdgeInsets.all(0),
+            elevation: 1,
+            expansionCallback: (int item, bool status) {
+              setState(() {
+                counselController.isExpanded[index] =
+                    !counselController.isExpanded[index];
+              });
+            },
+            children: [
+              ExpansionPanel(
+                backgroundColor: Colors.deepPurple.shade50,
+                headerBuilder: (BuildContext context, bool isExpanded) {
+                  return ListTile(
+                    title: Text(
+                      title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.deepPurple,
+                      ),
+                    ),
+                  );
+                },
+                body: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    details,
                     style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.deepPurple,
+                      color: Colors.black87,
                     ),
                   ),
-                );
-              },
-              body: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  details,
-                  style: TextStyle(
-                    color: Colors.black87,
-                  ),
                 ),
+                isExpanded: counselController.isExpanded[index],
               ),
-              isExpanded: _isExpanded[index],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
