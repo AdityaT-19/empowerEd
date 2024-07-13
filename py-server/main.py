bfb8f3f9d10a0df998d3a8251b894bd81258188e
AdityaT-19 <beanadi1910@gmail.com>
2024-07-14 02:43:16 +0530

    Refactor dependencies, add CORS middleware, and update pubspec.yaml

--- py-server/main.py
+++ py-server/main.py
@@ -14,11 +14,9 @@ from models.ctc_pred import Data
 
 app = FastAPI()
 
-origins = ["*"]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
